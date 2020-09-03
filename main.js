664f2d50bf97c13e9dc06ae4dff69fe886d91241
Bianca Reusch <reusch.bianca@gmail.com>
2020-09-03 09:26:06 -0500

    added styles

--- main.js
+++ main.js
@@ -193,3 +193,17 @@ function addCoffee(e){
     updateCoffees(e);
 }
 
+var coll = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+    coll[i].addEventListener("click", function() {
+        this.classList.toggle("active");
+        var content = this.nextElementSibling;
+        if (content.style.display === "block") {
+            content.style.display = "none";
+        } else {
+            content.style.display = "block";
+        }
+    });
+}
